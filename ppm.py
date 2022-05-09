f7f016812b9477333ed2c240c510487ff5578790
Alexander Nguyen <alexander.nguyen@stud.hfm-karlsruhe.de>
2022-05-09 17:27:46 +0200

    Facilitate PPM-Simple (previously: only PPM-Decay)

--- ppm.py
+++ ppm.py
@@ -1,14 +1,77 @@
 import os
 from .config import Config
-# R_HOME specifies the R instance to use by rpy2.
-os.environ["R_HOME"] = str(Config().r_home())
-
 import csv
 import pandas as pd
-from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
 from pathlib import Path
+from enum import Enum
 
-class PPMInputParameters:
+# R_HOME specifies the R instance to use by rpy2.
+# Needs to happen before any imports from rpy2
+os.environ["R_HOME"] = str(Config().r_home())
+from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
+
+
+class PPMEscapeMethod(Enum):
+    A = "a"
+    B = "b"
+    C = "c"
+    D = "d"
+    AX = "ax"
+
+class PPMSimpleInputParameters:
+    """This class represents all parameters of any PPM-Simple instance."""
+
+    def __init__(self, alphabet_size, order_bound, shortest_deterministic, exclusion, update_exclusion, escape: PPMEscapeMethod, debug_smooth, alphabet_levels):
+        self._alphabet_size = alphabet_size
+        self._order_bound = order_bound
+        self._shortest_deterministic = shortest_deterministic
+        self._exclusion = exclusion
+        self._update_exclusion = update_exclusion
+        self._escape = escape
+        self._debug_smooth = debug_smooth
+        self._alphabet_levels = alphabet_levels
+
+        self._output_parameters_file_path = None
+
+        self._sequence = None
+
+    def with_sequence(self, sequence):
+        self._sequence = sequence
+        return self
+
+    def with_output_parameters_file_path(self, output_parameters_file_path: Path):
+        self._output_parameters_file_path = output_parameters_file_path
+        return self
+
+    def write_csv(self, file_path: Path):
+        params = {
+            "model": "SIMPLE",
+            "order_bound": self._order_bound,
+            "alphabet_size": self._alphabet_size,
+            "alphabet_levels": list(self._alphabet_levels),
+            "shortest_deterministic": self._shortest_deterministic,
+            "exclusion": self._exclusion,
+            "update_exclusion": self._update_exclusion,
+            "escape": self._escape.value,
+            "debug_smooth": self._debug_smooth,
+
+            "input_sequence": self._sequence,
+            "output_parameters_file_path": str(self._output_parameters_file_path)
+        }
+
+        with open(file_path, 'w') as f:
+            csvwriter = csv.DictWriter(f, fieldnames=params.keys())
+            csvwriter.writeheader()
+            csvwriter.writerow(params)
+
+        return file_path
+
+    def read_csv(filename): # TODO implement import
+        return PPMSimpleInputParameters()
+
+class PPMDecayInputParameters:
+    """This class represents all parameters of any PPM-Decay instance."""
+
     def __init__(self, alphabet_size, order_bound, ltm_weight, ltm_half_life, ltm_asymptote, noise, stm_weight,
                  stm_duration, buffer_weight, buffer_length_time, buffer_length_items, only_learn_from_buffer,
                  only_predict_from_buffer, seed, debug_smooth, debug_decay, alphabet_levels):
@@ -33,9 +96,14 @@ class PPMInputParameters:
         self._output_parameters_file_path = None
 
         self._sequence = None
+        self._time_sequence = None
 
     def with_sequence(self, sequence):
         self._sequence = sequence
+        return self
+
+    def with_time_sequence(self, time_sequence: list):
+        self._time_sequence = time_sequence
         return self
 
     def with_output_parameters_file_path(self, output_parameters_file_path: Path):
@@ -44,8 +112,9 @@ class PPMInputParameters:
 
     def write_csv(self, file_path: Path):
         params = {
+            "model": "DECAY",
             "alphabet_size": self._alphabet_size,
-            "alphabet_levels": self._alphabet_levels,
+            "alphabet_levels": list(self._alphabet_levels),
             "order_bound": self._order_bound,
             "ltm_weight": self._ltm_weight,
             "ltm_half_life": self._ltm_half_life,
@@ -63,7 +132,7 @@ class PPMInputParameters:
             "debug_decay": self._debug_decay,
 
             "input_sequence": self._sequence,
-            "input_time_sequence": list(range(1, len(self._sequence) + 1)),  # ToDo: remove hard-code
+            "input_time_sequence": self._time_sequence,
             "output_parameters_file_path": str(self._output_parameters_file_path)
         }
 
@@ -74,12 +143,14 @@ class PPMInputParameters:
 
         return file_path
 
-    def read_csv(filename):
-        return PPMInputParameters()
+    def read_csv(filename): # TODO implement import
+        return PPMDecayInputParameters()
 
 
 class PPMOutputParameters:
-    def __init__(self, source_file_path, data_frame):
+    """This class represents an PPM output."""
+
+    def __init__(self, source_file_path: Path, data_frame: pd.DataFrame):
         self.source_file_path = source_file_path
         self.data_frame = pd.DataFrame.copy(data_frame)
 
@@ -87,11 +158,15 @@ class PPMOutputParameters:
         self.alphabet_size = len(self.data_frame['distribution_idx'].unique())
 
     def from_csv(file_path):
+        """Imports PPM output (.csv file)"""
+
         csv_data = pd.read_csv(file_path)
         return PPMOutputParameters(file_path, csv_data)
 
 
 class PPMInstance:
+    """This class represents a PPM instance."""
+
     PPM_RUN_FILEPATH = (Path(__file__).parent.absolute() / "./res/wrappers/ppm-decay/ppm_run.R").resolve()
 
     def __init__(self, ppmInputParameters, input_file_path: Path, output_file_path: Path):
@@ -107,15 +182,99 @@ class PPMInstance:
         package = SignatureTranslatedAnonymousPackage(r_file_contents, "ppm-python-bridge")
         return package
 
-    def observe(self, sequence):
-        self._ppmInputParameters.with_sequence(sequence).with_output_parameters_file_path(self.output_file_path).write_csv(
-            self.input_file_path)
+    def _generate_time_sequence(sequence):
+        return list(range(1, len(sequence) + 1))
+
+    def observe(self, sequence, time_sequence = None):
+        """ If [], and sequence is a list, a list with increasing numbers is generated, i.e. [1, 2, ..., len(sequence)]"""
+
+        if isinstance(self._ppmInputParameters, PPMDecayInputParameters):
+            if time_sequence is None or not isinstance(time_sequence, list):
+                raise ValueError("time_sequence invalid! Provide [], or any other list.")
+            if len(time_sequence) == 0:
+                time_sequence = PPMInstance._generate_time_sequence(sequence)
+            if len(time_sequence) != len(sequence):
+                raise ValueError("Length of sequence and time_sequence must be equal.")
+
+        self._ppmInputParameters.with_sequence(sequence).with_output_parameters_file_path(self.output_file_path)
+        if isinstance(self._ppmInputParameters, PPMDecayInputParameters):
+            self._ppmInputParameters.with_time_sequence(time_sequence)
+        self._ppmInputParameters.write_csv(self.input_file_path)
 
         result = self._r_script.run_ppm(str(self.input_file_path))
+        print(result)
         return self.output_file_path  # ToDo: return R output, which should contain the output_file_path.
 
+class PPMSimpleInstanceBuilder:
+    def __init__(self):
+        self._alphabet_size = None
+        self._order_bound = 10
+        self._shortest_deterministic = True
+        self._exclusion = True
+        self._update_exclusion = True
+        self._escape = PPMEscapeMethod.C
+        self._debug_smooth = False
+        self._alphabet_levels = {}
 
-class PPMInstanceBuilder:
+    def alphabet_size(self, alphabet_size):
+        if alphabet_size > 0:
+            self._alphabet_size = alphabet_size
+        else:
+            raise ValueError("Invalid alphabet_size! Value must be > 0.")
+        return self
+
+    def alphabet_levels(self, alphabet_levels: set):
+        if not isinstance(alphabet_levels, set):
+            raise ValueError("Invalid alphabet_levels! Value must be a set.")
+
+        self._alphabet_levels = alphabet_levels
+        self.alphabet_size(len(alphabet_levels))
+
+        return self
+
+    def order_bound(self, order_bound):
+        self._order_bound = order_bound
+
+    def shortest_deterministic(self, shortest_deterministic):
+        self._shortest_deterministic = shortest_deterministic
+
+    def exclusion(self, exclusion):
+        self._exclusion = exclusion
+
+    def update_exclusion(self, update_exclusion):
+        self._update_exclusion = update_exclusion
+
+    def escape(self, escape: PPMEscapeMethod):
+        if not isinstance(escape, PPMEscapeMethod):
+            raise ValueError("escape invalid! It must be member of PPMEscapeMethod enum.")
+        self._escape = escape
+
+    def debug_smooth(self, debug_smooth):
+        self._debug_smooth = debug_smooth
+
+    def with_input_file_path(self, input_file_path: Path):
+        self.input_file_path = input_file_path
+
+        return self
+
+    def with_output_file_path(self, output_file_path: Path):
+        self.output_file_path = output_file_path
+
+        return self
+
+    def build(self):
+        if self.input_file_path is None:
+            raise Exception("input_file_path required!")
+        if self.output_file_path is None:
+            raise Exception("output_file_path required!")
+        if self._alphabet_size is None or self._alphabet_size == 0:
+            raise Exception("Invalid alphabet_size (or alphabet_levels)! Value (or count) must be > 0.")
+
+        ppmInputParameters = PPMSimpleInputParameters(self._alphabet_size, self._order_bound, self._shortest_deterministic, self._exclusion, self._update_exclusion, self._escape, self._debug_smooth, self._alphabet_levels)
+        return PPMInstance(ppmInputParameters, self.input_file_path, self.output_file_path)
+
+
+class PPMDecayInstanceBuilder:
     def __init__(self):
         self._alphabet_size = None
         self._order_bound = 10
@@ -130,10 +289,10 @@ class PPMInstanceBuilder:
         self._buffer_length_items = 0
         self._only_learn_from_buffer = False
         self._only_predict_from_buffer = False
-        self._seed = 1
+        self._seed = 1 # TODO make random as in original source?
         self._debug_smooth = False
         self._debug_decay = False
-        self._alphabet_levels = []
+        self._alphabet_levels = {}
 
         self.input_file_path = None
         self.output_file_path = None
@@ -145,12 +304,13 @@ class PPMInstanceBuilder:
             raise ValueError("Invalid alphabet_size! Value must be > 0.")
         return self
 
-    def alphabet_levels(self, alphabet_levels):
-        if isinstance(alphabet_levels, set):
-            self._alphabet_levels = alphabet_levels
-            self.alphabet_size(len(alphabet_levels))
-        else:
+    def alphabet_levels(self, alphabet_levels: set):
+        if not isinstance(alphabet_levels, set):
             raise ValueError("Invalid alphabet_levels! Value must be a set.")
+
+        self._alphabet_levels = alphabet_levels
+        self.alphabet_size(len(alphabet_levels))
+
         return self
 
     def order_bound(self, order_bound):
@@ -228,15 +388,14 @@ class PPMInstanceBuilder:
             raise Exception("input_file_path required!")
         if self.output_file_path is None:
             raise Exception("output_file_path required!")
+        if self._alphabet_size is None or self._alphabet_size == 0:
+            raise Exception("Invalid alphabet_size (or alphabet_levels)! Value (or count) must be > 0.")
 
-        if self._alphabet_size != None and self._alphabet_size > 0:
-            ppmInputParameters = PPMInputParameters(self._alphabet_size, self._order_bound, self._ltm_weight,
-                                                    self._ltm_half_life, self._ltm_asymptote, self._noise,
-                                                    self._stm_weight, self._stm_duration, self._buffer_weight,
-                                                    self._buffer_length_time, self._buffer_length_items,
-                                                    self._only_learn_from_buffer, self._only_predict_from_buffer,
-                                                    self._seed, self._debug_smooth, self._debug_decay,
-                                                    self._alphabet_levels)
-            return PPMInstance(ppmInputParameters, self.input_file_path, self.output_file_path)
-        else:
-            raise ValueError("Invalid alphabet_size! Value must be > 0.")
+        ppmInputParameters = PPMDecayInputParameters(self._alphabet_size, self._order_bound, self._ltm_weight,
+                                                     self._ltm_half_life, self._ltm_asymptote, self._noise,
+                                                     self._stm_weight, self._stm_duration, self._buffer_weight,
+                                                     self._buffer_length_time, self._buffer_length_items,
+                                                     self._only_learn_from_buffer, self._only_predict_from_buffer,
+                                                     self._seed, self._debug_smooth, self._debug_decay,
+                                                     self._alphabet_levels)
+        return PPMInstance(ppmInputParameters, self.input_file_path, self.output_file_path)
--- res/wrappers/ppm-decay/ppm_run.R
+++ res/wrappers/ppm-decay/ppm_run.R
@@ -3,53 +3,81 @@ library(ppm)
 library(dplyr, warn.conflicts=FALSE)
 library(tidyr)
 run_ppm <- function(input_file_path) {
-params <- read.csv(input_file_path)
 
-params$only_learn_from_buffer <- as.logical(params$only_learn_from_buffer)
-params$only_predict_from_buffer <- as.logical(params$only_predict_from_buffer)
-params$debug_decay <- as.logical(params$debug_decay)
-params$debug_smooth <- as.logical(params$debug_smooth)
+  # read instructions
+  params <- read.csv(input_file_path)
+  requested_model <- params$model
+  
+  # map logical data types
+  if (requested_model == "DECAY") {
+    params$only_learn_from_buffer <- as.logical(params$only_learn_from_buffer)
+    params$only_predict_from_buffer <- as.logical(params$only_predict_from_buffer)
+    params$debug_decay <- as.logical(params$debug_decay)
+  } else if (requested_model == "SIMPLE") {
+    params$shortest_deterministic <- as.logical(params$shortest_deterministic)
+    params$exclusion <- as.logical(params$exclusion)
+    params$update_exclusion <- as.logical(params$update_exclusion)
+  }
+  params$debug_smooth <- as.logical(params$debug_smooth)
 
-alphabet_levels <- strsplit(gsub("\\[(.*?)\\]", "\\1", params$alphabet_levels), ",\\s*")[[1]]
-if (length(alphabet_levels) > 0) {
-  alphabet_levels <- as.factor(alphabet_levels)
-} else {
-  alphabet_levels <- character()
-}
-input_sequence <- strsplit(gsub("\\[(.*?)\\]", "\\1", params$input_sequence), ",\\s*")[[1]]
-if (length(alphabet_levels) > 0) {
-  input_sequence <- as.factor(input_sequence, levels = params$alphabet_levels)
-} else {
-  input_sequence <- as.factor(input_sequence)
-}
-input_time_seq <- strsplit(gsub("\\[(.*?)\\]", "\\1", params$input_time_sequence), ",\\s*")[[1]]
-input_time_seq <- as.numeric(input_time_seq)
-
-########################
-
-mod <- new_ppm_decay(params$alphabet_size, 
-                     order_bound = params$order_bound, 
-                     ltm_weight = params$ltm_weight, ltm_half_life = params$ltm_half_life, ltm_asymptote = params$ltm_asymptote, 
-                     noise = params$noise, 
-                     stm_weight = params$stm_weight, stm_duration = params$stm_duration, 
-                     buffer_weight = params$buffer_weight, buffer_length_time = params$buffer_length_time, 
-                     buffer_length_items = params$buffer_length_items, 
-                     only_learn_from_buffer = params$only_learn_from_buffer, 
-                     only_predict_from_buffer = params$only_predict_from_buffer, 
-                     seed = params$seed, 
-                     debug_smooth = params$debug_smooth, 
-                     debug_decay = params$debug_decay, 
-                     alphabet_levels = alphabet_levels
-                     )
-res <- model_seq(mod, input_sequence, time=input_time_seq)
-res <- res %>% mutate(observation = levels(input_sequence)[symbol])
-#########################
-
-ppm_res <- res %>% mutate(symbol_idx = row_number()) %>% 
-  unnest_longer(distribution, indices_to = "distribution_idx") %>% 
-  rowwise() %>% mutate(distribution_symbol = levels(input_sequence)[distribution_idx]) %>%
-  select(symbol_idx, distribution_idx, distribution_symbol, distribution, everything())
-
-write.csv(ppm_res, params$output_parameters_file_path)
-params$output_csv_file
+  
+  # parse alphabet_levels
+  alphabet_levels <- strsplit(gsub("\\[(.*?)\\]", "\\1", params$alphabet_levels), ",\\s*")[[1]]
+  if (length(alphabet_levels) == 0) {
+    alphabet_levels <- character()
+  }
+  
+  # parse input_sequence
+  input_sequence <- strsplit(gsub("\\[(.*?)\\]", "\\1", params$input_sequence), ",\\s*")[[1]]
+  if (length(alphabet_levels) > 0) {
+    input_sequence <- factor(input_sequence, levels = alphabet_levels)
+  } else {
+    input_sequence <- as.factor(input_sequence)
+  }
+  
+  # parse input_time_seq (if DECAY)
+  if (requested_model == "DECAY") {
+    input_time_seq <- strsplit(gsub("\\[(.*?)\\]", "\\1", params$input_time_sequence), ",\\s*")[[1]]
+    input_time_seq <- as.numeric(input_time_seq)
+  }
+  
+  ########################
+  if (requested_model == "DECAY") {
+    mod <- new_ppm_decay(params$alphabet_size, 
+                         order_bound = params$order_bound, 
+                         ltm_weight = params$ltm_weight, ltm_half_life = params$ltm_half_life, ltm_asymptote = params$ltm_asymptote, 
+                         noise = params$noise, 
+                         stm_weight = params$stm_weight, stm_duration = params$stm_duration, 
+                         buffer_weight = params$buffer_weight, buffer_length_time = params$buffer_length_time, 
+                         buffer_length_items = params$buffer_length_items, 
+                         only_learn_from_buffer = params$only_learn_from_buffer, 
+                         only_predict_from_buffer = params$only_predict_from_buffer, 
+                         seed = params$seed, 
+                         debug_smooth = params$debug_smooth, 
+                         debug_decay = params$debug_decay, 
+                         alphabet_levels = alphabet_levels
+                         )
+    res <- model_seq(mod, input_sequence, time=input_time_seq)
+  } else if (requested_model == "SIMPLE") {
+    mod <- new_ppm_simple(params$alphabet_size, 
+                          order_bound = params$order_bound,
+                          shortest_deterministic = params$shortest_deterministic,
+                          exclusion = params$exclusion,
+                          update_exclusion = params$update_exclusion,
+                          escape = params$escape,
+                          debug_smooth = params$debug_smooth,
+                          alphabet_levels = alphabet_levels)
+    res <- model_seq(mod, input_sequence)
+  }
+  res <- res %>% mutate(observation = levels(input_sequence)[symbol])
+  #########################
+  
+  ppm_res <- res %>% mutate(symbol_idx = row_number()) %>% 
+    unnest_longer(distribution, indices_to = "distribution_idx") %>% 
+    rowwise() %>% mutate(distribution_symbol = levels(input_sequence)[distribution_idx]) %>%
+    select(symbol_idx, distribution_idx, distribution_symbol, distribution, everything())
+  
+  write.csv(ppm_res, params$output_parameters_file_path)
+  
+  return(params$output_parameters_file_path)
 }
