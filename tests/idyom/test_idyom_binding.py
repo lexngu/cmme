8bfb751c601213ae7123d80f5974534bb041343b
Alexander Nguyen <alexander.nguyen@stud.hfm-karlsruhe.de>
2023-09-15 08:21:49 +0200

    Fix test

--- tests/idyom/test_idyom_binding.py
+++ tests/idyom/test_idyom_binding.py
@@ -81,63 +81,64 @@ def test_load_idyom_instructions_file_minimal():
         assert test_idyom_if.output_options == output_options
 
 def test_load_idyom_instructions_file_with_more_parameters():
-    idyom_root_path = "/tmp/test123"
-    idyom_database_path = "/tmp/test123/db/database.sqlite"
-    dataset = 1
-    model = IDYOMModelType.BOTH_PLUS
-    source_viewpoints = "cpitch"
-    target_viewpoints = "cpitch"
-    stm_options = {
-        "order_bound": 3,
-        "mixtures": False,
-        "update_exclusion": None,
-        "escape": IDYOMEscapeMethod.A,
-    }
-    ltm_options = {
-        "order_bound": 4,
-        "mixtures": None,
-        "update_exclusion": True,
-        "escape": None
-    }
-    training_options = {
-        "pretraining_dataset_ids": [2, 5, 6],
-        "resampling_folds_count_k": 5,
-        "exclusively_to_be_used_resampling_fold_indices": [1, 2, 3]
-    }
-    output_path = path_as_string_with_trailing_slash(tempfile.TemporaryDirectory().name)
-    output_options = {
-        "output_path": output_path,
-        "detail": 3,
-        "overwrite": True,
-        "separator": " "
-    }
-    idyom_ib = IDYOMInstructionBuilder()\
-        .idyom_root_path(idyom_root_path)\
-        .idyom_database_path(idyom_database_path)\
-        .dataset(dataset)\
-        .source_viewpoints(source_viewpoints)\
-        .target_viewpoints(target_viewpoints)\
-        .model(model)\
-        .stm_options(**stm_options)\
-        .ltm_options(**ltm_options)\
-        .training_options(**training_options)\
-        .output_options(**output_options)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        idyom_root_path = os.path.join(tmpdir, "test123")
+        idyom_database_path = os.path.join(tmpdir, "test123/db/database.sqlite")
+        dataset = 1
+        model = IDYOMModelType.BOTH_PLUS
+        source_viewpoints = "cpitch"
+        target_viewpoints = "cpitch"
+        stm_options = {
+            "order_bound": 3,
+            "mixtures": False,
+            "update_exclusion": None,
+            "escape": IDYOMEscapeMethod.A,
+        }
+        ltm_options = {
+            "order_bound": 4,
+            "mixtures": None,
+            "update_exclusion": True,
+            "escape": None
+        }
+        training_options = {
+            "pretraining_dataset_ids": [2, 5, 6],
+            "resampling_folds_count_k": 5,
+            "exclusively_to_be_used_resampling_fold_indices": [1, 2, 3]
+        }
+        output_path = path_as_string_with_trailing_slash(tempfile.TemporaryDirectory().name)
+        output_options = {
+            "output_path": output_path,
+            "detail": 3,
+            "overwrite": True,
+            "separator": " "
+        }
+        idyom_ib = IDYOMInstructionBuilder()\
+            .idyom_root_path(idyom_root_path)\
+            .idyom_database_path(idyom_database_path)\
+            .dataset(dataset)\
+            .source_viewpoints(source_viewpoints)\
+            .target_viewpoints(target_viewpoints)\
+            .model(model)\
+            .stm_options(**stm_options)\
+            .ltm_options(**ltm_options)\
+            .training_options(**training_options)\
+            .output_options(**output_options)
 
-    with tempfile.NamedTemporaryFile() as tmpfile:
-        idyom_if = idyom_ib.to_instructions_file()
-        idyom_if.save_self(tmpfile.name)
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            idyom_if = idyom_ib.to_instructions_file()
+            idyom_if.save_self(tmpfile.name)
 
-        test_idyom_if = IDYOMInstructionsFile.load(tmpfile.name)
-        assert test_idyom_if.idyom_root_path == idyom_root_path
-        assert test_idyom_if.idyom_database_path == idyom_database_path
-        assert test_idyom_if.dataset == dataset
-        assert test_idyom_if.source_viewpoints == transform_string_list_to_viewpoints_list(source_viewpoints)
-        assert test_idyom_if.target_viewpoints == transform_string_list_to_viewpoints_list(target_viewpoints)
-        assert test_idyom_if.model == IDYOMModelType(model).value
-        assert test_idyom_if.stm_options == stm_options
-        assert test_idyom_if.ltm_options == ltm_options
-        assert test_idyom_if.training_options == training_options
-        assert test_idyom_if.output_options == output_options
+            test_idyom_if = IDYOMInstructionsFile.load(tmpfile.name)
+            assert test_idyom_if.idyom_root_path == path_as_string_with_trailing_slash(idyom_root_path)
+            assert test_idyom_if.idyom_database_path == idyom_database_path
+            assert test_idyom_if.dataset == dataset
+            assert test_idyom_if.source_viewpoints == transform_string_list_to_viewpoints_list(source_viewpoints)
+            assert test_idyom_if.target_viewpoints == transform_string_list_to_viewpoints_list(target_viewpoints)
+            assert test_idyom_if.model == IDYOMModelType(model).value
+            assert test_idyom_if.stm_options == stm_options
+            assert test_idyom_if.ltm_options == ltm_options
+            assert test_idyom_if.training_options == training_options
+            assert test_idyom_if.output_options == output_options
 
 def test_load_results_file():
     idyom_root_path = Config().idyom_root_path()
